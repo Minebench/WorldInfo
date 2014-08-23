5420be563633bc78088792e6c9dc6a2b0b4cb2c8
Christopher Krier <ckrier.3000@gmail.com>
2014-08-22 20:12:14 -0400

    Edit Settings to lock in plugin channel.
    	Also manually loads the config from now on. Instead of using plugin.getConfig();

--- src/main/java/tk/coaster3000/worldinfo/Settings.java
+++ src/main/java/tk/coaster3000/worldinfo/Settings.java
@@ -1,5 +1,6 @@
 package tk.coaster3000.worldinfo;
 
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import tk.coaster3000.worldinfo.bukkit.WorldInfoPlugin;
 
@@ -15,8 +16,8 @@ public class Settings {
 	private static final String ENCODING_NODE = "encoding";
 	private static final String ID_MODE_NODE = "mode";
 	private static final String INFORM_PLAYER_NODE = "inform-player";
+	private static final String PLUGIN_CHANNEL = "world_info";
 
-	private String channel = "world_info";
 	private String encoding = "UTF-8";
 	private ID_MODE id_mode = ID_MODE.NAME;
 
@@ -42,12 +43,7 @@ public class Settings {
 	}
 
 	public static String channel() {
-		return getInstance().channel;
-	}
-
-	public Settings channel(String channel) {
-		this.channel = channel;
-		return this;
+		return PLUGIN_CHANNEL;
 	}
 
 	public static String encoding() {
@@ -93,10 +89,15 @@ public class Settings {
 	}
 
 	public Settings load() {
-		config = plugin.getConfig(); //Reload config automatically.
+		try {
+			config.load(configFile); //Reload config automatically.
+		} catch (IOException e) {
+			log.log(Level.WARNING, "IO Exception Occured... Could not load config..", e);
+		} catch (InvalidConfigurationException e) {
+			log.log(Level.SEVERE, "Invalid configuration file supplied. Did you put tabs in it?!", e);
+		}
 
 		inform_player(config.getBoolean("inform-player", false));
-		channel(config.getString("plugin-channel", "world_info"));
 		encoding(config.getString("encoding", "UTF-8"));
 		id_mode(ID_MODE.valueOf(config.getString("mode", ID_MODE.NAME.name()).toUpperCase(Locale.ENGLISH)));
 
