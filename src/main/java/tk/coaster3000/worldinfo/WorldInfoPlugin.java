f11a658d67f532fe3b55a4beb7055d3ea359b937
Teruteru <teruterubouzu1024@gmail.com>
2019-12-29 14:45:15 +0900

    Upgrade source version

--- src/main/java/tk/coaster3000/worldinfo/WorldInfoPlugin.java
+++ src/main/java/tk/coaster3000/worldinfo/WorldInfoPlugin.java
@@ -1,6 +1,5 @@
 package tk.coaster3000.worldinfo;
 
-import com.google.common.io.LineReader;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
@@ -17,6 +16,7 @@ import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Locale;
 import java.util.UUID;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class WorldInfoPlugin extends JavaPlugin implements PluginMessageListener, Listener {
@@ -61,15 +62,13 @@ public class WorldInfoPlugin extends JavaPlugin implements PluginMessageListener
 			try {
 				config.save(configFile);
 			} catch (IOException e) {
-				e.printStackTrace();
+				log.log(Level.WARNING, "reload save failed", e);
 			}
 		} else {
 			try {
 				config.load(configFile);
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (InvalidConfigurationException e) {
-				e.printStackTrace();
+			} catch (IOException | InvalidConfigurationException e) {
+				log.log(Level.WARNING, "reload save failed", e);
 			}
 		}
 		config.options().copyDefaults(true);
@@ -113,24 +112,15 @@ public class WorldInfoPlugin extends JavaPlugin implements PluginMessageListener
 	}
 
 	private void genID(File idFile) {
-		FileWriter fw = null;
 		try {
 			boolean newFile = idFile.createNewFile();
 			if (newFile) {
-				fw = new FileWriter(idFile);
-				fw.write(UUID.randomUUID().toString());
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (fw != null) {
-				try {
-					fw.close();
-				} catch (IOException e) {
-					log.warning("Failed to close file '" + idFile.getPath() + "' from writing...");
-					e.printStackTrace();
+				try(FileWriter fw = new FileWriter(idFile)){
+					fw.write(UUID.randomUUID().toString());
 				}
 			}
+		} catch (IOException e) {
+			log.log(Level.WARNING, "", e);
 		}
 	}
 
@@ -149,33 +139,18 @@ public class WorldInfoPlugin extends JavaPlugin implements PluginMessageListener
 		player.sendPluginMessage(this, channel, buffer.array());
 	}
 
-
-
 	private String getFileID(World w) {
 		String ret = null;
-		FileReader fr = null;
 
 		File file = new File(w.getWorldFolder(), "world.id");
-		try {
-			fr = new FileReader(file);
-			LineReader lr = new LineReader(fr);
-			ret = lr.readLine();
+		try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			ret = reader.readLine();
 		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (fr != null) {
-				try {
-					fr.close();
-				} catch (IOException e) {
-					log.warning("Attempted to close file '" + file.getPath() + "' but failed.");
-					e.printStackTrace();
-				}
-			}
+			log.log(Level.WARNING, "", e);
 		}
 		return ret;
 	}
 
-	@Override
 	public void onPluginMessageReceived(String channel, Player player,
 	                                    byte[] bytes) {
 		log.info("Message received");
@@ -189,9 +164,7 @@ public class WorldInfoPlugin extends JavaPlugin implements PluginMessageListener
 				player.sendMessage("Message recieved and sending data...");
 			}
 
-
 			World w = player.getWorld();
-
 
 			byte[] data = ZERO_BYTES;
 			switch (idMode) {
@@ -208,7 +181,7 @@ public class WorldInfoPlugin extends JavaPlugin implements PluginMessageListener
 
 			sendData(player, (byte) 0, data);
 		} catch (Throwable t) {
-			t.printStackTrace();
+			log.log(Level.WARNING, "", t);
 			if (informPlayer && player.isOnline()) player.sendMessage(new String[]{
 					ChatColor.RED + "An error occurred sending world UUID!",
 					ChatColor.GRAY + "Please check server logs for details..."
